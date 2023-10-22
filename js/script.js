d81c22db152c22223edb90505e9d75adcf809f9d
Guilherme Silva <143155079+guiidevs@users.noreply.github.com>
2023-10-22 20:48:59 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+
+const opc = document.querySelector(".opcoes");
+let seletor = 0;
+let limite = 0;
+
+function deslizar(e){
+    if(e == "right" && limite <= 1){
+        limite = limite + 1
+        seletor = seletor + 100;
+        opc.style.transform = "translateX(-" + seletor + "px)";
+    }
+    if(e == "left" && seletor != 0){
+        limite = limite - 1
+        seletor = seletor - 100;
+        opc.style.transform = "translateX(-" + seletor + "px)";
+    }
+};
